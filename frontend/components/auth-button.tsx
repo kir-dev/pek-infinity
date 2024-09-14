2a3f1067734673de36841a7fb832b6057907ece4
Tamás Südi <sudi.tamas@simonyi.bme.hu>
2024-09-14 02:10:17 +0200

    fix login url

--- frontend/components/auth-button.tsx
+++ frontend/components/auth-button.tsx
@@ -1,21 +1,23 @@
 import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
+import { getBackend } from '@/lib/get-backend';
 import { authMe } from '@/pek-api';
 
 export async function AuthButton() {
+  const baseUrl = getBackend({ preferredNetwork: 'public' });
   try {
     await authMe();
   } catch {
     return (
       <Button asChild>
-        <Link href='http://localhost:3300/api/v4/auth/login'>Login</Link>
+        <Link href={`${baseUrl}/api/v4/auth/login`}>Login</Link>
       </Button>
     );
   }
   return (
     <Button asChild>
-      <Link href='http://localhost:3300/api/v4/auth/logout'>Logout</Link>
+      <Link href={`${baseUrl}/api/v4/auth/logout`}>Logout</Link>
     </Button>
   );
 }
