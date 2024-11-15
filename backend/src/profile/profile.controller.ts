53d3b8a4ec10588eb88232eda5c3233d00c16cc1
Németh Marcell <nemeth.marcell.nm@gmail.com>
2024-11-15 21:57:44 +0100

    updateBasic and updatePersonal controller

--- backend/src/profile/profile.controller.ts
+++ backend/src/profile/profile.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProfileService } from './profile.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { UpdateBasicsDto } from '@/profile/dto/update-basics.dto';
+import { UpdatePersonalDto } from '@/profile/dto/update-personal.dto';
+import { UpdateProfileNameDto } from '@/profile/dto/update-profile-name.dto';
+
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { ProfileService } from './profile.service';
 
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @Post()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   create(@Body() createProfileDto: CreateProfileDto) {
-    return this.profileService.create(createProfileDto);
+    return null;
+    //return this.profileService.create(createProfileDto);
   }
 
   @Get()
@@ -19,16 +34,34 @@ export class ProfileController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.profileService.findOne(+id);
+    return this.profileService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
-    return this.profileService.update(+id, updateProfileDto);
+  //TODO
+  @Patch('profilename/:id')
+  update(
+    @Param('id') id: string,
+    @Body() updateProfileNameDto: UpdateProfileNameDto,
+  ) {
+    return null;
+    //return this.profileService.updateProfileName(id, updateProfileNameDto);
+  }
+
+  @Patch('personal/:id')
+  update(
+    @Param('id') id: string,
+    @Body() updatePersonalDto: UpdatePersonalDto,
+  ) {
+    return this.profileService.updatePersonal(id, updatePersonalDto);
+  }
+
+  @Patch('basics/:id')
+  update(@Param('id') id: string, @Body() updateBasicsDto: UpdateBasicsDto) {
+    return this.profileService.updateBasic(id, updateBasicsDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.profileService.remove(+id);
+    return this.profileService.remove(Number(id));
   }
 }
