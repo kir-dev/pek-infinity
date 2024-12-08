856a79a48f63dcdf79b1b47d45df3e37f907e631
Tamás Südi <sudi.tamas@simonyi.bme.hu>
2024-12-08 21:17:46 +0100

    Refactor ApiController decorator

---
+++ backend/src/ping/ping.controller.ts
@@ -0,0 +1,21 @@
+import { Get } from '@nestjs/common';
+
+import { ApiController } from '@/utils/controller.decorator';
+
+import { Ping } from './entities/ping.entity';
+
+@ApiController('ping', { authStrategy: 'UNRESTRICTED' })
+export class PingController {
+  /**
+   * # Health check endpoint<br>
+   *
+   * This endpoint is a simple health check API designed to confirm that the server is operational.
+   *
+   * When accessed, it returns a straightforward response indicating that the service is up and running.
+   *
+   */
+  @Get()
+  send(): Ping {
+    return { ping: 'PONG' };
+  }
+}
