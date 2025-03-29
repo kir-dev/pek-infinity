035f88ef9a44de6ed99e00a118b45a37a37c88dd
Tamás Südi <sudi.tamas@simonyi.bme.hu>
2025-03-30 00:33:11 +0100

    chore: migrate from ESLint and Prettier to Biome for linting and formatting

--- backend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['../.eslintrc.js'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-};
