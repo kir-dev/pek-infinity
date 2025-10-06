88573ab6c076c9b63b6ece84d983e864dfe24d33
Tamás Südi <sudi.tamas@simonyi.bme.hu>
2025-10-06 02:30:10 +0200

    chore(prisma): enable Prisma client generation
    - Enable Prisma Client generation and configuration.
    - Add prisma.config.mjs
    - Add npm scripts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { GroupModule } from './group/group.module';
 import { MembershipModule } from './membership/membership.module';
 import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [UserModule, GroupModule, MembershipModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
