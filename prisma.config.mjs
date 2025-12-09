9c1083cde83b08a19e6d89f0a8d551cd3753ea40
Tamás Südi <sudi.tamas@simonyi.bme.hu>
2025-12-09 23:33:49 +0100

    Upgrade to prisma 7

--- prisma.config.mjs
+++ prisma.config.mjs
@@ -1,5 +1,14 @@
-import { defineConfig } from 'prisma/config';
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: './prisma',
+  schema: 'prisma/',
+  migrations: {
+    path: 'prisma/migrations',
+    // seed: 'tsx prisma/seed.ts',
+  },
+  datasource: {
+    url: env('POSTGRES_PRISMA_URL'),
+    // shadowDatabaseUrl: env('SHADOW_DATABASE_URL'),
+  },
 });
